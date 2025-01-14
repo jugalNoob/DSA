ad1d3904cfce644e1a991416854c76d717406262
jugal sharma <78578433+jugalNoob@users.noreply.github.com>
2025-01-13 21:35:38 -0800

    Update Logic2.jsx

--- DsaCode/logic/Logic2.jsx
+++ DsaCode/logic/Logic2.jsx
@@ -82,6 +82,11 @@ function flipButton() {
 
 // Memoization function .....................:::::::::::::::::::::::::::::::
 
+            // console.time("First call array");
+// let arr=[10 , 20 , 30 , 40 , 50]
+// console.log(arr)
+// console.timeEnd("First call array");
+
 function sum(one) {
    let total = 0;
    for (let i = 1; i <= one; i++) {
