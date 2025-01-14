6f4521addea51a86ac4bb161803bddea90c76dde
jugal sharma <78578433+jugalNoob@users.noreply.github.com>
2025-01-14 04:41:35 -0800

    Create logic3.jsx

---
+++ DsaCode/logic/logic3.jsx
@@ -0,0 +1,63 @@
+
+// compact array value  .....................:::::::::::::::::::::::::::
+const compact=(array)=>{
+   return array.filter(Boolean)
+}
+
+console.log(compact([1, 2, '', 3, '', 4, 5])) // [1, 2, 3, 4, 5]
+
+
+
+// Repeating without Loop  ...................................:::::::::
+console.log("1".repeat(4));
+
+// Random Number Gen ...............................:::::::::::::::::
+
+const random = (min, max) => {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+ };
+ 
+ // Example usage
+ const min = 5;
+ const max = 15;
+ const randomNumber = random(min, max);
+ console.log(randomNumber); // Output: A random number between 5 and 15 (inclusive)
+ 
+
+// Digitize Number to array .................................::::::::::::::::::::::::
+const convertToArray = (number) => {
+   return [...`${number}`].map(el => parseInt(el));
+ };
+ 
+ // Example usage
+ const number = 12345;
+ const digitArray = convertToArray(number);
+ console.log(digitArray); // Output: [1, 2, 3, 4, 5]
+ 
+
+
+ // revers string without methods  ..............................::::::::::::::::::::::::
+
+let string="jugal sharma"
+
+let reversedstring=""
+
+for(let index=string.length-1; index >= 0; index--){
+
+   reversedstring+=string[index]
+}
+
+console.log(reversedstring); // Output:
+
+
+
+//Ternary Operator .....................:::::::::::::::::::
+
+
+
+const isloggedIn=true
+
+const button=isloggedIn ? "log out" : "log in"
+
+
+console.log(button)
